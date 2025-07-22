50ed5a553bbb6f7f5f39476213d6751c02842ff6
Jonathan Zambrano <jzambrano.cant@gmail.com>
2025-07-22 11:15:54 -0500

    Fix Plantilla icons

--- bolsillo_pro/resources/views/layouts/app.blade.php
+++ bolsillo_pro/resources/views/layouts/app.blade.php
@@ -613,13 +613,13 @@
             fill: #000
         }
     </style>
+    
     <link href="https://unicons.iconscout.com/release/v4.0.8/css/line.css" rel="stylesheet">
     <link rel="apple-touch-icon" sizes="180x180" href="https://prium.github.io/phoenix/v1.23.0/assets/img/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="https://prium.github.io/phoenix/v1.23.0/assets/img/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="https://prium.github.io/phoenix/v1.23.0/assets/img/favicons/favicon-16x16.png">
     <link rel="shortcut icon" type="image/x-icon" href="https://prium.github.io/phoenix/v1.23.0/assets/img/favicons/favicon.ico">
     <link rel="manifest" href="https://prium.github.io/phoenix/v1.23.0/assets/img/favicons/manifest.json">
-    <meta name="msapplication-TileImage" content="../../../assets/img/favicons/mstile-150x150.png">
     <meta name="theme-color" content="#ffffff">
     <script src="../resources/phoenix/js/simplebar.min.js"></script>
     <script src="../resources/phoenix/js/config.js"></script>
@@ -636,7 +636,7 @@
     <link href="../resources/phoenix/css/theme.min.css" type="text/css" rel="stylesheet" id="style-default">
     <link href="../resources/phoenix/css/user-rtl.min.css" type="text/css" rel="stylesheet" id="user-style-rtl" disabled="true">
     <link href="../resources/phoenix/css/user.min.css" type="text/css" rel="stylesheet" id="user-style-default">
-    
+
     <script>
         var phoenixIsRTL = window.config.config.phoenixIsRTL;
         if (phoenixIsRTL) {
